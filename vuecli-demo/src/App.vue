bd5ae49884a9d72ead651cbda2346a2c8cd88bd2
whutfaker <1312976124@qq.com>
2020-02-21 17:44:15 +0800

    打卡day1

---
+++ vuecli-demo/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!-- 调用组件 -->
+    <Users />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+//1.局部调用组件
+import Users from "./components/Users";
+
+
+export default {
+  name: 'App',
+  //局部注册组件
+  components:{//Vue.component
+    Users
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuecli-demo/src/components/Users.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="users">
+    nihao
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style >
+
+</style>
