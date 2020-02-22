598d08bb5e131799a195acf3af507ade90eba80e
whutfaker <1312976124@qq.com>
2020-02-22 22:38:53 +0800

    又是美好的一天结束

--- vuecli-demo/src/App.vue
+++ vuecli-demo/src/App.vue
@@ -2,31 +2,48 @@
   <div id="app">
     <!-- 调用组件 -->
     <Header></Header>
-    <Users />
+    <Users :users="users" />
     <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 //1.局部调用组件
 import Header from "./components/Header";
 import Users from "./components/Users";
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 
 export default {
-  name: 'App',
+  name: "App",
   //局部注册组件
-  components:{//Vue.component
+  data() {
+    return {
+       users: [
+        { name: "AX1", wechat: "123456", show: false },
+        { name: "AX2", wechat: "123456", show: false },
+        { name: "AX3", wechat: "123456", show: false },
+        { name: "AX4", wechat: "123456", show: false },
+        { name: "AX5", wechat: "123456", show: false },
+        { name: "AX6", wechat: "123456", show: false },
+        { name: "AX7", wechat: "123456", show: false },
+        { name: "AX8", wechat: "123456", show: false },
+        { name: "AX9", wechat: "123456", show: false }
+      ]
+    };
+  },
+
+  components: {
+    //Vue.component
     Users,
     Header,
     Footer
   }
-}
+};
 </script>
 //注释
 <style scoped>
-h1{
+h1 {
   color: purple;
 }
 </style>
--- vuecli-demo/src/components/Users.vue
+++ vuecli-demo/src/components/Users.vue
@@ -3,55 +3,51 @@
     <h1>Users.vue</h1>
     <ul>
       <li v-for="(user,index) in users" :key="index" @click="user.show=!user.show">
-          <h2>我的名字：{{user.name}}</h2>
-          <p v-if="user.show">我的微信号：{{user.wechat}}</p>
-          </li>
+        <h2>我的名字：{{user.name}}</h2>
+        <p v-if="user.show">我的微信号：{{user.wechat}}</p>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
+  props: ["users"],
   data() {
     return {
-      users: [
-        { name: "AX1", wechat: "123456", show: false },
-        { name: "AX2", wechat: "123456", show: false },
-        { name: "AX3", wechat: "123456", show: false },
-        { name: "AX4", wechat: "123456", show: false },
-        { name: "AX5", wechat: "123456", show: false },
-        { name: "AX6", wechat: "123456", show: false },
-        { name: "AX7", wechat: "123456", show: false },
-        { name: "AX8", wechat: "123456", show: false },
-        { name: "AX9", wechat: "123456", show: false }
-      ]
-    };
-  }
+      users
+    }
+  },
+  methods: {
+      test(){
+          console.log(this.users)
+      }
+  },
 };
 </script>
 
 <style scoped>
-.user{
-    width: 100%;
-    max-width: 1200px;
-    margin: 40px auto;
-    padding: 0 20px;
-    box-sizing: border-box;
+.user {
+  width: 100%;
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
-ul{
-    display: flex;
-    flex-wrap: wrap;
-    list-style-type: none;
-    padding: 0
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
 }
-li{
-    flex-grow: 1;
-    flex-basis: 200px;
-    text-align: center;
-    padding: 30px;
-    border:1px solid #222;
-    margin: 10px;
-    cursor: pointer;
+li {
+  flex-grow: 1;
+  flex-basis: 200px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+  margin: 10px;
+  cursor: pointer;
 }
 h1 {
   color: aqua;
