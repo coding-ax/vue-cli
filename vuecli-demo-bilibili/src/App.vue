f4cd8aba381333886992b83ec2ac80f9f4a5c97c
whutfaker <1312976124@qq.com>
2020-02-27 20:34:06 +0800

    第四天，心态有点乱

--- vuecli-demo-bilibili/src/App.vue
+++ vuecli-demo-bilibili/src/App.vue
@@ -1,13 +1,14 @@
+
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button >你好</Button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
--- vuecli-demo-bilibili/src/main.js
+++ vuecli-demo-bilibili/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//产品提示信息：(开发阶段不显示，默认false)
 Vue.config.productionTip = false
 
 new Vue({
