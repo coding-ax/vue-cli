61c459178c537fb06be2391e05394175092e7efe
whutfaker <1312976124@qq.com>
2020-03-10 21:47:07 +0800

    开始vuex

---
+++ vuexlearn/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h2>我是APP组件</h2>
+    <h2>{{this.$store.state.count}}</h2>
+    <button @click="$store.state.count++">+</button>
+    <button @click="$store.state.count--">-</button>
+    <hello-vuex></hello-vuex>
+  </div>
+</template>
+
+<script>
+import HelloVuex from './components/HelloVuex.vue'
+export default {
+  components:{
+    HelloVuex
+  },
+  data() {
+    return {
+      message:'helloVueX'
+    }
+  },
+  
+}
+</script>
+<style>
+
+</style>
---
+++ vuexlearn/src/components/HelloVuex.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h2>我是HelloVueX</h2>
+      <h2>{{this.$store.state.count}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
