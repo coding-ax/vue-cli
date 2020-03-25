73d2ae287abacefe86e14d59596ec1b0f4b53c13
whutfaker <1312976124@qq.com>
2020-03-25 21:13:20 +0800

    学习了怎么初步使用饿了么UI“

---
+++ ui-learn/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    good bye 
+      <paging></paging>    
+  </div>
+</template>
+
+<script>
+import paging from './components/paging.vue';
+export default {
+  name: "App",
+  components: {
+    paging
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ ui-learn/src/components/paging.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
