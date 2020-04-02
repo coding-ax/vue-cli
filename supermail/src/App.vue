4ffbf66e8ec418d120dfd461f3cb15d5aedb338a
whutfaker <1312976124@qq.com>
2020-04-02 12:04:49 +0800

    跟随做supermail

--- supermail/src/App.vue
+++ supermail/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-      <paging></paging>    
   </div>
 </template>
 
 <script>
-import paging from './components/paging.vue';
+
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    paging
   }
-};
+}
 </script>
 
 <style>
